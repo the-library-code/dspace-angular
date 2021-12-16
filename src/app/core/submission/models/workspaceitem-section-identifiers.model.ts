bf46561dbb62a217ce49abb25eb08092e09908c1
Kim Shepherd <kim@shepherd.nz>
2021-12-16 16:37:23 +1300

    [TLC-249] Add comments / doc and tidy up component, template, model

--- src/app/core/submission/models/workspaceitem-section-identifiers.model.ts
+++ src/app/core/submission/models/workspaceitem-section-identifiers.model.ts
@@ -1,7 +1,8 @@
-import { Item } from '../../shared/item.model';
-
+/*
+ * Object model for the data returned by the REST API to present minted identifiers in a submission section
+ */
 export interface WorkspaceitemSectionIdentifiersObject {
-  doi: string
-  handle: string
-  identifiers: string[]
+  doi?: string
+  handle?: string
+  otherIdentifiers?: string[]
 }
--- src/app/submission/sections/identifiers/section-identifiers.component.ts
+++ src/app/submission/sections/identifiers/section-identifiers.component.ts
@@ -1,16 +1,13 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Inject, Input} from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 
 import { SectionsType } from '../sections-type';
 import { SectionModelComponent } from '../models/section.model';
 import { renderSectionFor } from '../sections-decorator';
 import { SectionDataObject } from '../models/section-data.model';
-import { PaginationComponentOptions } from '../../../shared/pagination/pagination-component-options.model';
 import { SubmissionService } from '../../submission.service';
-import { SubmissionScopeType } from '../../../core/submission/submission-scope-type';
 import { AlertType } from '../../../shared/alert/aletr-type';
 import { SectionsService } from '../sections.service';
 import { WorkspaceitemSectionIdentifiersObject } from '../../../core/submission/models/workspaceitem-section-identifiers.model';
@@ -18,7 +15,11 @@ import { PaginationService } from '../../../core/pagination/pagination.service';
 import { SubmissionVisibility } from '../../utils/visibility.util';
 
 /**
- * This component represents a section that contains possible duplications.
+ * This simple component displays DOI, handle and other identifiers that are already minted for the item in
+ * a workflow / submission section.
+ * ShowMintIdentifierStep will attempt to reserve an identifier before injecting result data for this component.
+ *
+ * @author Kim Shepherd
  */
 @Component({
   selector: 'ds-submission-section-identifiers',
@@ -41,28 +42,10 @@ export class SubmissionSectionIdentifiersComponent extends SectionModelComponent
   public isLoading = true;
 
   /**
-   * The object containing data about doi, handle, other identifiers
-   * @type {Observable}
-   */
-  public sectionData$: Observable<WorkspaceitemSectionIdentifiersObject>;
-
-  /**
-   * The pagination system configuration for HTML listing.
-   * @type {PaginationComponentOptions}
-   */
-  config$: Observable<PaginationComponentOptions>;
-
-  /**
    * If TRUE the submission scope is the 'workflow'; 'workspace' otherwise.
    * @type {boolean}
    */
   isWorkFlow = false;
-
-  /**
-   * The list of the possible duplications.
-   * @type {PaginationComponentOptions}
-   */
-  disclaimer: Observable<string>;
 
   /**
    * Initialize instance variables.
@@ -75,8 +58,17 @@ export class SubmissionSectionIdentifiersComponent extends SectionModelComponent
    * @param {SectionDataObject} injectedSectionData
    * @param {string} injectedSubmissionId
    */
+  /*
   constructor(protected paginationService: PaginationService,
               protected translate: TranslateService,
+              protected sectionService: SectionsService,
+              protected submissionService: SubmissionService,
+              @Inject('collectionIdProvider') public injectedCollectionId: string,
+              @Inject('sectionDataProvider') public injectedSectionData: SectionDataObject,
+              @Inject('submissionIdProvider') public injectedSubmissionId: string) {
+
+   */
+  constructor(protected translate: TranslateService,
               protected sectionService: SectionsService,
               protected submissionService: SubmissionService,
               @Inject('collectionIdProvider') public injectedCollectionId: string,
@@ -89,44 +81,7 @@ export class SubmissionSectionIdentifiersComponent extends SectionModelComponent
    * Initialize all instance variables and retrieve configuration.
    */
   onSectionInit() {
-    const config = new PaginationComponentOptions();
-    config.id = 'identifiers';
-    config.pageSize = 2;
-    config.pageSizeOptions = [1, 2, 5];
-    this.config$ = this.paginationService.getCurrentPagination(config.id, config);
-
-    if (this.submissionService.getSubmissionScope() === SubmissionScopeType.WorkflowItem) {
-      this.isWorkFlow = true;
-    } else {
-      this.isWorkFlow = false;
-    }
-
     this.isLoading = false;
-  }
-
-  /**
-   * Get section status.
-   *
-   * @return Observable<boolean>
-   *     the section status
-   */
-  public getSectionStatus(): Observable<boolean> {
-    return this.sectionData$.pipe(
-      map((totalMatches: any) => {
-        let output = false;
-        if (Object.keys(totalMatches.matches).length === 0) {
-          output = true;
-        }
-        return output;
-      })
-    );
-  }
-
-  /**
-   * The data of this section.
-   */
-  get data(): WorkspaceitemSectionIdentifiersObject {
-    return this.sectionData.data as WorkspaceitemSectionIdentifiersObject;
   }
 
   /**
@@ -144,6 +99,19 @@ export class SubmissionSectionIdentifiersComponent extends SectionModelComponent
    */
   onSectionDestroy(): void {
     return;
+  }
+
+  // TODO: better logic here instead of "always true"
+  getSectionStatus(): Observable<boolean> {
+    return true as Observable<boolean>;
+  }
+
+  /**
+   * Get identifier data (from the REST service) as a simple object with doi, handle, otherIdentifiers variables
+   * and as an observable so it can update in real-time.
+   */
+  getIdentifierData() {
+    return this.sectionService.getSectionData(this.submissionId, this.sectionData.id, this.sectionData.sectionType) as Observable<WorkspaceitemSectionIdentifiersObject>
   }
 
 }
