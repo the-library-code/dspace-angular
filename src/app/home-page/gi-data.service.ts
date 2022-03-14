cc51774d1907875267ace68e875dadf88c6098da
Tim Kuntze <tim@MacBook-Pro-von-Tim.local>
2022-03-14 17:52:10 +0100

    [TLC-221] Added service for gi-specific requirements

---
+++ src/app/home-page/gi-data.service.ts
@@ -0,0 +1,18 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from '@angular/core';
+
+@Injectable()
+
+export class GiDataService {
+
+  constructor() {
+  }
+
+  /**
+   * Get the endpoint for browsing items
+   *  (When options.sort.field is empty, the default field to browse by will be 'dc.date.issued')
+   * @param {FindListOptions} options
+   * @param linkPath
+   * @returns {Observable<string>}
+   */
+}
