ac8035e80798eb2084b049f0b794d217236a849e
Tim Kuntze <tim@MBP-von-Tim.fritz.box>
2022-03-15 11:56:16 +0100

    [TLC-221] Overall basic design of homepage components

--- src/app/home-page/gi-data.service.ts
+++ src/app/home-page/gi-data.service.ts
@@ -1,11 +1,13 @@
-import {HttpClient} from "@angular/common/http";
-import {Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 
 export class GiDataService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   /**
--- src/app/home-page/gi-fields/gi-fields.component.ts
+++ src/app/home-page/gi-fields/gi-fields.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GiDataService } from '../gi-data.service';
 
 @Component({
   selector: 'ds-gi-fields',
