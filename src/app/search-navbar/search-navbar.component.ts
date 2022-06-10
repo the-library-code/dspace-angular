99ab05878a447060f2589d9647f32ef9d40540b6
Peter Wolfersberger <peter@The-Library-Code.de>
2022-06-10 16:20:05 +0200

    [TLC 221] search navbar fixed
    search navbar again closer to dspace original code. Behavior in interaction with "discover" optimized using dsClickOutside. Search field closing animation disabled because of <form [hidden]="!searchExpanded ...". If animation is desired, this attribute link has to be deleted, but an invisible search input field 24px (padding) wide will remain left to the search menu entry.

--- src/app/search-navbar/search-navbar.component.ts
+++ src/app/search-navbar/search-navbar.component.ts
@@ -1,12 +1,93 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SearchService } from '../core/shared/search/search.service';
+import { expandSearchInput } from '../shared/animations/slide';
+import { PaginationService } from '../core/pagination/pagination.service';
+import { SearchConfigurationService } from '../core/shared/search/search-configuration.service';
+import {GiDataService} from '../home-page/gi-data.service';
+
+/**
+ * The search box in the header that expands on focus and collapses on focus out
+ */
+@Component({
+  selector: 'ds-search-navbar',
+  templateUrl: './search-navbar.component.html',
+  styleUrls: ['./search-navbar.component.scss'],
+  animations: [expandSearchInput]
+})
+export class SearchNavbarComponent implements OnInit {
+
+  // The search form
+  searchForm;
+  // Whether or not the search bar is expanded, boolean for html ngIf, string fo AngularAnimation state change
+  searchExpanded = false;
+  isExpanded = 'collapsed';
+  searchPlaceholder;
+
+  // Search input field
+  @ViewChild('searchInput') searchField: ElementRef;
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private searchService: SearchService,
+              private paginationService: PaginationService,
+              private searchConfig: SearchConfigurationService, public gds: GiDataService) {
+    this.searchForm = this.formBuilder.group(({
+      query: '',
+    }));
+  }
+
+  /**
+   * Expands search bar by angular animation, see expandSearchInput
+   */
+  expand() {
+    this.searchExpanded = true;
+    this.isExpanded = 'expanded';
+    this.editSearch();
+  }
+
+  /**
+   * Collapses & blurs search bar by angular animation, see expandSearchInput
+   */
+  collapse() {
+    this.searchField.nativeElement.blur();
+    this.isExpanded = 'collapsed';
+    this.searchExpanded = false;
+  }
+
+  /**
+   * Focuses on input search bar so search can be edited
+   */
+  editSearch(): void {
+    this.searchField.nativeElement.focus();
+  }
+
+  /**
+   * Submits the search (on enter or on search icon click)
+   * @param data  Data for the searchForm, containing the search query
+   */
+  onSubmit(data: any) {
+    this.collapse();
+    const linkToNavigateTo = this.searchService.getSearchLink().split('/');
+    this.searchForm.reset();
+    this.paginationService.updateRouteWithUrl(this.searchConfig.paginationID, linkToNavigateTo, {page: 1}, data);
+  }
+
+  ngOnInit() {
+    this.searchPlaceholder = this.gds.searchPlaceholder;
+  }
+}
+
+
+/*
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SearchService } from '../core/shared/search/search.service';
 import { slide } from '../shared/animations/slide';
 
-/**
+/!**
  * The search box in the header that expands on focus and collapses on focus out
- */
+ *!/
 @Component({
   selector: 'ds-search-navbar',
   templateUrl: './search-navbar.component.html',
@@ -30,9 +111,9 @@ export class SearchNavbarComponent {
     }));
   }
 
-  /**
+  /!**
    * Expands search bar by angular animation, see expandSearchInput
-   */
+   *!/
   expand() {
     this.searchExpanded = true;
     this.isExpanded = 'expanded';
@@ -40,9 +121,9 @@ export class SearchNavbarComponent {
     (document.querySelector('body') as HTMLElement).style.overflowY = 'hidden';
   }
 
-  /**
+  /!**
    * Collapses & blurs search bar by angular animation, see expandSearchInput
-   */
+   *!/
   collapse() {
     this.searchField.nativeElement.blur();
     this.searchExpanded = false;
@@ -50,17 +131,17 @@ export class SearchNavbarComponent {
     (document.querySelector('body') as HTMLElement).style.overflowY = 'visible';
   }
 
-  /**
+  /!**
    * Focuses on input search bar so search can be edited
-   */
+   *!/
   editSearch(): void {
     this.searchField.nativeElement.focus();
   }
 
-  /**
+  /!**
    * Submits the search (on enter or on search icon click)
    * @param data  Data for the searchForm, containing the search query
-   */
+   *!/
   onSubmit(data: any) {
     this.collapse();
     const queryParams = Object.assign({}, data);
@@ -73,3 +154,4 @@ export class SearchNavbarComponent {
     });
   }
 }
+*/
