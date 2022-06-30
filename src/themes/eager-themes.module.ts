a6d3b9aea7fb9bdfcc29e237828dec608a0c14d6
Peter Wolfersberger <peter@The-Library-Code.de>
2022-06-30 13:42:54 +0200

    Copied custom theme as base for gi theme.
    Also changed angular.json, config.yml, src/themes/eager-themes.module.ts accordingly (see https://wiki.lyrasis.org/display/DSDOC7x/User+Interface+Customization).
    Also added "host": "0.0.0.0" to the "serve" section in angular.json

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
-// import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
+// import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
+import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
+import { EagerThemeModule as GiEagerThemeModule } from './gi/eager-theme.module';
 
 /**
  * This module bundles the eager theme modules for all available themes.
@@ -11,8 +12,9 @@ import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme
  */
 @NgModule({
   imports: [
-    DSpaceEagerThemeModule,
-    // CustomEagerThemeModule,
+    // DSpaceEagerThemeModule,
+    GiEagerThemeModule,
+    CustomEagerThemeModule,
   ],
 })
 export class EagerThemesModule {
