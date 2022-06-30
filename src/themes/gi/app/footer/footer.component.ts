5be40458fe9bd42267685ab4f71ecf6085ac22b4
Peter Wolfersberger <peter@The-Library-Code.de>
2022-06-30 18:24:07 +0200

    integrated footer

--- src/themes/gi/app/footer/footer.component.ts
+++ src/themes/gi/app/footer/footer.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.component';
+import { Component, OnInit, Optional } from '@angular/core';
+import { hasValue } from '../../../../app/shared/empty.util';
+import { KlaroService } from '../../../../app/shared/cookies/klaro.service';
+import { GiDataService } from '../shared/gi-data.service';
 
 @Component({
   selector: 'ds-footer',
-  // styleUrls: ['footer.component.scss'],
-  styleUrls: ['../../../../app/footer/footer.component.scss'],
-  // templateUrl: 'footer.component.html'
-  templateUrl: '../../../../app/footer/footer.component.html'
+  styleUrls: ['footer.component.scss'],
+  templateUrl: 'footer.component.html'
 })
-export class FooterComponent extends BaseComponent {
+export class FooterComponent implements OnInit {
+  dateObj: number = Date.now();
+
+  /**
+   * A boolean representing if to show or not the top footer container
+   */
+  showTopFooter = true;
+
+  req;
+
+  constructor(@Optional() private cookies: KlaroService, private gidata: GiDataService) {
+  }
+
+  ngOnInit() {
+    this.req = this.gidata.uiConfigreq;
+  }
+
+  showCookieSettings() {
+    if (hasValue(this.cookies)) {
+      this.cookies.showSettings();
+    }
+    return false;
+  }
+
 }
