d927ec3d03c8a4cdec9ed556e1ea2f254f98e05d
Peter Wolfersberger <peter@The-Library-Code.de>
2022-07-01 10:49:11 +0200

    integrate imprint component into gi theme

---
+++ src/themes/gi/app/info/imprint/imprint.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-imprint',
+  styleUrls: ['./imprint.component.scss'],
+  // styleUrls: ['../../../../../app/info/imprint/imprint.component.scss'],
+  templateUrl: './imprint.component.html'
+  // templateUrl: '../../../../../app/info/imprint/imprint.component.html'
+})
+
+/**
+ * Component displaying the Privacy Statement
+ */
+export class ImprintComponent {}
