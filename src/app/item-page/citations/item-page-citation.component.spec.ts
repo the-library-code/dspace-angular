5f93e5c86a2208e99361e60d869f50002f57f4c6
Kim Shepherd <kim@shepherd.nz>
2022-09-04 12:17:41 +1200

    [TLC-337] small template changes, start unit tests

---
+++ src/app/item-page/citations/item-page-citation.component.spec.ts
@@ -0,0 +1,8 @@
+import { ItemPageCitationComponent } from './item-page-citation.component';
+import { ComponentFixture } from '@angular/core/testing';
+
+describe('ItemPageCitationComponent spec tests', () => {
+  let comp: ItemPageCitationComponent;
+  let fixture: ComponentFixture<ItemPageCitationComponent>;
+
+})
